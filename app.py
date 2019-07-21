12678d887ed6afa4ff5ea4f8f1f47be2bc24ba3d
Cassandra Heide <cassandraheide@gmail.com>
2019-07-21 18:36:48 -0400

    add data source local csv

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+data = pd.read_csv('~/code/intenergy/all_energy_statistics_2019.csv')
+
+# just for testing! have to convert units!
+totals1990 = pd.DataFrame(data[data['Year']==1990].groupby(by=['Country or Area'])['Quantity'].sum())
+totals2016 = pd.DataFrame(data[data['Year']==2016].groupby(by=['Country or Area'])['Quantity'].sum())
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='International Energy Statistics'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Source: United Nations Statistics Division (UNSD): http://data.un.org/Explorer.aspx.
     '''),
 
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': totals1990.index, 'y': totals1990['Quantity'], 'type': 'bar', 'name': '1990 Total Energy'},
+                {'x': totals2016.index, 'y': totals2016['Quantity'], 'type': 'bar', 'name': '2016 Total Energy'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'title': 'Example Graph - Total Energy 1990 vs 2016 (units tbd)'
             }
         }
     )
